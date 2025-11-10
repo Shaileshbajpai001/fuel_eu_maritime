7876bfc26abe51f2a41aa5d36f44862da24a9c38
Shailesh Bajpai <shaileshshaileshbajpai4488@gmail.com>
2025-11-10 13:05:14 +0530

    Initial Project Setup

---
+++ frontend/src/App.tsx
@@ -0,0 +1,21 @@
+// import { useState } from 'react'
+// import reactLogo from './assets/react.svg'
+// import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  // const [count, setCount] = useState(0)
+
+  return (
+    <>
+    <div className='place-content-center justify-center px-40'>
+
+      <p className="read-the-docs text-red-600 text-center justify-around text-8xl place-items-baseline">
+        Click Sumit pal
+      </p>
+    </div>
+    </>
+  )
+}
+
+export default App
